5600a1b128ff2085d0cb2c26c90ecc3875a1d349
Milos Belcevic <65423284+mbelcevic@users.noreply.github.com>
2024-06-27 22:38:35 +0200

    Update cointoss.py
    This version of the script adds a counter (user_wins) that increments each time the user wins a coin toss. When the user chooses to stop playing, the script will display the total number of wins before exiting.

--- cointoss.py
+++ cointoss.py
@@ -1,7 +1,7 @@
 import random
 
 def coin_toss():
-    user_guess = input("Please choose HEADS or TAILS").upper()
+    user_guess = input("Please choose HEADS or TAILS: ").upper()
     while user_guess not in ["HEADS", "TAILS"]:
             user_guess = input("Please type in 'HEADS' or 'TAILS': ").upper()
             
@@ -17,17 +17,21 @@ def coin_toss():
         
     if user_guess == result:
         print("You win!")
+        return True
     else:
-        print("You loose")
+        print("You lose")
+        return False
 
 def main():
+    user_wins = 0
     while True:
-        coin_toss()
+        if coin_toss():
+            user_wins += 1
         play_again = input("Do you want to play again? Type 'Y' for yes or 'N' for no: ").upper()
         while play_again not in ["Y", "N"]:
             play_again = input("Please type 'Y' for yes or 'N' for no: ").upper()
         if play_again == "N":
-            print("Thanks for playing. Goodbye!")
+            print(f"Thanks for playing. You won {user_wins} times. Goodbye!")
             break
 
 if __name__ == "__main__":
